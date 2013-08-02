b57d2d3d0c20f3b3910b747699d3254ba7888131
yjroot <yjroot@gmail.com>
2013-08-02 22:13:42 +0900

    Fixed config bug.

--- config.py
+++ config.py
@@ -4,32 +4,33 @@ config = ConfigParser.RawConfigParser()
 config.read('config.ini')
 
 class Config:
-	def __init__(self, section_name):
-		self.section_name = section_name
-		if not config.has_section(section_name):
-			config.add_section(section_name)
-			self.save()
+    def __init__(self, section_name):
+    	self.section_name = section_name
+    	if not config.has_section(section_name):
+    		config.add_section(section_name)
+    		self.save()
 
-	def save(self):
-		with open('config.ini', 'wb') as configfile:
-			config.write(configfile)
+    def save(self):
+    	with open('config.ini', 'wb') as configfile:
+    		config.write(configfile)
 
-	def all(self):
-		return config.items(self.section_name)
+    def all(self):
+    	return config.items(self.section_name)
 
-	def get(self, key, default=None):
-		result = config.get(self.section_name, key)
-		if result:
-			return result
-		return default 
+    def get(self, key, default=None):
+        try:
+            result = config.get(self.section_name, key)
+        except:
+            result = default
+    	return result or default 
 
-	def set(self, key, value):
-		config.set(self.section_name, key, value)
-		self.save()
-	
-	def __getitem__(self, key):
-		return self.get(key)
+    def set(self, key, value):
+    	config.set(self.section_name, key, value)
+    	self.save()
+    
+    def __getitem__(self, key):
+    	return self.get(key)
 
-	def __setitem__(self, key, value):
-		self.set(key, value)
+    def __setitem__(self, key, value):
+    	self.set(key, value)
 
